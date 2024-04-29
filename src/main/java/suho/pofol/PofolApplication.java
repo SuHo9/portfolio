1af1373256ed8cc42a75fff72053badb23aa7a41
SuHo9 <suho930303@gmail.com>
2024-04-29 13:07:03 +0900

    init

---
+++ src/main/java/suho/pofol/PofolApplication.java
@@ -0,0 +1,13 @@
+package suho.pofol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PofolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PofolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/suho/pofol/PofolApplicationTests.java
@@ -0,0 +1,13 @@
+package suho.pofol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PofolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
